cf928690062d5c5aa1af9d55a02c644f923099e1
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2018-11-27 22:35:46 -0500

    checked in final project

---
+++ TinyMCEDemo/Controllers/BooksController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyMCEDemo.Models;
+
+namespace TinyMCEDemo.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Book book)
+        {
+            //try
+            //{
+            //    // TODO: Add insert logic here
+
+            //   // return RedirectToAction(nameof(Index));
+            //}
+            //catch
+            //{
+            return View(book);
+
+        }
+
+        // GET: Books/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Books/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TinyMCEDemo/wwwroot/js/site.js
+++ TinyMCEDemo/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your JavaScript code.
+﻿(function ($) {
+    "use strict";
+    $(document).ready(function () {
+        tinymce.init({
+            selector: 'textarea'
+        });
+
+    });
+})(this.jQuery);
