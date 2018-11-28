8ed2db0ad4e23aab8a4130ca27e6b4d32d8a6df4
Bekenty Jean Baptiste <bekenty.baptiste@outlook.com>
2018-11-27 21:02:35 -0500

    Add project files.

---
+++ TinyMCEDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TinyMCEDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
